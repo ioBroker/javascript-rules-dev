384f99687705c2ff99a0bdb8214a765089ea389f
GermanBluefox <dogafox@gmail.com>
2025-01-15 08:16:34 +0000

    Added GenericBlockProps

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export type * from './types';
 export { default as GenericBlock } from './GenericBlock';
-export { IGenericBlock, type GenericBlockProps } from './IGenericBlock';
+export { IGenericBlock, type GenericBlockProps, type GenericBlockState } from './IGenericBlock';
